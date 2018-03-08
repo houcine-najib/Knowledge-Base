95f7a2fb3af00697b5a391c671f7bcdee6de2e69
houcine-najib <37178707+houcine-najib@users.noreply.github.com>
2018-03-08 12:10:58 +0100

    Create loadmem.c

---
+++ loadmem.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    unsigned long mem;
+    if(argc==1)
+      {
+        mem = 1024*1024*256;
+        printf("Using Default : Allocating 256 MB\n");
+        int i;
+     for( i = 1; i <=256; i++ ){
+         printf("Allocating : %d MB\n", i);
+         mem = 1024*1024*i;
+         char* ptr = malloc(mem);
+         memset(ptr, 0, mem);
+         sleep(1);
+        }
+      }
+    else if(argc==2)
+       {
+        printf("Allocating %s MBytes in Total\n",argv[1]);
+        int i;
+     for( i = 1; i <=(unsigned) atol(argv[1]); i++ ){
+         printf("Allocating : %d MB\n", i);
+         mem = 1024*1024*i;
+         char* ptr = malloc(mem);
+         memset(ptr, 0, mem);
+         sleep(1);
+        }
+       }
+    else
+    {
+        printf("Usage: loadmem <memory in bytes>");
+        exit(1);
+    }
+
+     while(1)
+    {
+        sleep(120);
+    }
+}
