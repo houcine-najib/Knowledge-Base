4c68c45fc8c17b2e011b136b061eaf4b6a7eba50
houcine-najib <najib.houcine@gmail.com>
2018-07-03 23:07:58 +0100

    03-07-2018

--- linux/tuned/profile-script.sh
+++ linux/tuned/profile-script.sh
@@ -8,14 +8,17 @@
 start() {
 
 echo "Profile Traning got loaded" >> /tmp/tuned.log
-
-}
+# 
+disable_bluetooth
+enable_wifi_powersave
 
 # exectued at profile unload
 stop() {
-
+#
 echo  "Profile Training got unloaded" >> /tmp/tuned.log
 
+# Power Saving Disbale
+enable_bluetooth
+disable_wifi_powersave
 }
-
 process $@
